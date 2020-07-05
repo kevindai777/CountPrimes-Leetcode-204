4cc569c9315cdf67a83bb89e34394828b41a6c47
kevindai777 <kevin.dai.2451@gmail.com>
2020-07-04 21:00:44 -0400

    solutions

---
+++ countprimes.js
@@ -0,0 +1,19 @@
+//Objective is to count the number of primes up to 'n'.
+
+let n = 10
+
+
+//O(nlognlogn) solution that uses the Sieve of Eratosthenes
+
+let primes = new Array(n).fill(true)
+
+for (let i = 2; i * i < n; i++) {
+    if (primes[i]) {
+        //Mark all multiples if 'i' false
+        for (let j = i; j * i < n; j++) {
+            primes[i * j] = false
+        } 
+    }
+}
+
+return primes.filter(boolean => boolean).length
